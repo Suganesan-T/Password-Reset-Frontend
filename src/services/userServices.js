b46df1046b722570d440b75cbc18f1dfb0f4132b
Suganesan-T <suganesan71@gmail.com>
2024-05-04 01:18:05 +0530

    updated

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -23,7 +23,7 @@ const userService = {
     },
     logout: async()=>{
         //make a post request to the logout enpoints
-        return await protectInstance.get('/users/logout');
+        return await instance.get('/users/logout');
     },
     getUser: async()=>{
         //make a get request to the get user enpoints
