122385a3ea7a7c51e58190087bfcf34cf77fa519
Suganesan-T <suganesan71@gmail.com>
2024-05-03 23:35:31 +0530

    added  forget-password

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Signup from './Signup'
 import Forgetpass from './Forgetpass'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './home'
+import Resetpassword from './Resetpassword'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path='/forget-password' element={<Forgetpass/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
+        <Route path='/reset-password' element = {<Resetpassword/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
--- src/Forgetpass.jsx
+++ src/Forgetpass.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import userService from './services/userServices'
 
 function Forgetpass() {
 
+    const [email,setEmail] = useState("")
+
+    const handleForgetPassword = (e) => {
+        e.preventDefault()
+        userService.forgetPassword(email)
+        .then(responce => {
+            console.log(res)
+            alert("check your email")
+            setEmail("")
+            setTimeout(() => {
+                navigate("/reset-password")
+            }, 500);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
 
 
 
@@ -12,12 +30,12 @@ function Forgetpass() {
             <div className="row">
                 <div className="col-lg-12 d-flex justify-content-center alihn items center">
                     <div className="form-signin w-50 m-auto">
-                        <form >
+                        <form onSubmit={handleForgetPassword} >
                            
                             <h1 className="h3 mb-3 fw-normal text-center mt-5">Forget Password?</h1>
                             <div className="form-floating">
-                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" 
-                                
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+                                    value={email} onChange={(e) => setEmail(e.target.value)} 
                                 />
                                 <label for="floatingInput">Email address</label>
 
