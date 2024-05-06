bc6d3be98dbbdd06cfa750a1eba5aca5baa8f264
Suganesan-T <suganesan71@gmail.com>
2024-05-06 17:58:08 +0530

    updated

--- src/Resetpassword.jsx
+++ src/Resetpassword.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 function Resetpassword() {
 
