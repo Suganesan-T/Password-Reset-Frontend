fe173ff601b254da009c0ffa23db18d20ab57e6e
Suganesan-T <suganesan71@gmail.com>
2024-05-06 12:27:26 +0530

    updated

--- src/Resetpassword.jsx
+++ src/Resetpassword.jsx
@@ -1,48 +1,40 @@
+import axios from 'axios';
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function Resetpassword() {
 
     let { resetToken } = useParams();
-    const [newPassword, setNewPassword] = useState('');
-  const [resetStatus, setResetStatus] = useState(null);
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate()
 
-  const handleResetPassword = async () => {
-    try {
-      // Send a request to your backend to reset the password
-      const response = await fetch('/api/resetPassword', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          resetToken,
-          password: newPassword,
-        }),
-      });
-
-      if (response.ok) {
-        setResetStatus('success');
-      } else {
-        setResetStatus('error');
-      }
-    } catch (error) {
-      console.error('Error resetting password:', error);
-      setResetStatus('error');
+    axios.defaults.withCredentials = true;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.put(`http://password-reset-backend-sz6n.onrender.com/api/users/resetPassword/${resetToken}`, { password })
+            .then(res => {
+                if (res.data.status === 'success') {
+                    navigate('/login')
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
-  };
-  return (
-    <div classNameName="container">
+
+
+    return (
+        <div classNameName="container">
             <div classNameName="row">
                 <div classNameName="col-lg-12 d-flex justify-content-center alihn items center">
                     <div classNameName="form-signin w-50 m-auto">
-                        <form onSubmit={handleResetPassword} >
-                           
+                        <form onSubmit={handleSubmit} >
+
                             <h1 classNameName="h3 mb-3 fw-normal text-center mt-5">Reset Your Password</h1>
                             <div classNameName="form-floating">
                                 <input type="password" classNameName="form-control" id="floatingPassword" placeholder="password"
-                                    value={newPassword} onChange={(e) => setNewPassword(e.target.value)}
-                                    
+                                  value={password}  onChange={(e) => setPassword(e.target.value)}
+
                                 />
                                 <label for="floatingPassword">Enter new password</label>
 
@@ -53,7 +45,7 @@ function Resetpassword() {
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default Resetpassword
