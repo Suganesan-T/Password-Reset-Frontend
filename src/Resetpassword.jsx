964aa82de6a4278867b0a47716a8d2dfe37e5867
Suganesan-T <suganesan71@gmail.com>
2024-05-06 18:03:03 +0530

    Added reset password

--- src/Resetpassword.jsx
+++ src/Resetpassword.jsx
@@ -30,17 +30,14 @@ function Resetpassword() {
             <div className="row">
                 <div className="col-lg-12 d-flex justify-content-center alihn items center">
                     <div className="form-signin w-50 m-auto">
-                        <form  >
-                           
-                            <h1 className="h3 mb-3 fw-normal text-center mt-5">Forget Password?</h1>
-                            <div className="form-floating">
-                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
-                                    
+                        <form onSubmit={handleSubmit}  >
+                            <h1 className="h3 mb-3 fw-normal text-center mt-5">Reset Your Password</h1>
+                            <div className="form-floating mt-3">
+                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                                    onChange={(e) => setPassword(e.target.value)}
                                 />
-                                <label for="floatingInput">Email address</label>
-
+                                <label for="floatingPassword">Password</label>
                                 <button className="btn btn-primary w-100 py-2 mt-4 " type="submit">submit</button>
-                                <p className="mt-3 mb-3 text-body-secondary"> <Link to="/">Login</Link></p>
                             </div>
                         </form>
                     </div>
